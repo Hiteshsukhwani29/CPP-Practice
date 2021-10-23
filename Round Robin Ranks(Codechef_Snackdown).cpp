8da7ee58ec20dae6ef9a80a44950d16a79f60ae1
Hitesh Sukhwani <55627571+Hiteshsukhwani29@users.noreply.github.com>
2021-10-23 22:49:04 +0530

    Create Round Robin Ranks(Codechef_Snackdown).cpp

---
+++ Round Robin Ranks(Codechef_Snackdown).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b;
+	    c=(2*a-b-1)/2;
+	    cout<<c*2<<endl;
+	}
+	return 0;
+}
