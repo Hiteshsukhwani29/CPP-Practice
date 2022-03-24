27fc260e699dfe6c2c431e164fb0a74e7a4c1208
Hitesh Sukhwani <55627571+Hiteshsukhwani29@users.noreply.github.com>
2022-03-24 10:43:32 -0700

    Create transition_point_gfg.cpp

---
+++ transition_point_gfg.cpp
@@ -0,0 +1,10 @@
+int transitionPoint(int arr[], int n) {
+    // code here
+    for(int i=0;i<n;i++){
+        if(arr[i]==1){
+            return i;
+            break;
+        }
+    }
+    return -1;
+}
