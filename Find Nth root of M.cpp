8f3f1a7692cfc49dc2f1199eb627f6d767139711
Hitesh Sukhwani <55627571+Hiteshsukhwani29@users.noreply.github.com>
2022-05-30 22:20:31 +0530

    Create Find Nth root of M.cpp

---
+++ Find Nth root of M.cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+	public:
+	int NthRoot(int n, int m)
+	{
+	    long int a=0,b=0;
+	    while(b<=m){
+	        b=pow(a,n);
+	        if(b==m){
+	            return a;
+	        }
+	        a++;
+	    }
+	    return -1;
+	}  
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		Solution ob;
+		int ans = ob.NthRoot(n, m);
+		cout << ans << "\n";
+	}  
+	return 0;
+}  // } Driver Code Ends
