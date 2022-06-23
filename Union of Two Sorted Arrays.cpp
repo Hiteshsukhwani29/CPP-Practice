001564f8283e068d9fd108b6edf5851232af8be3
Hitesh Sukhwani <55627571+Hiteshsukhwani29@users.noreply.github.com>
2022-06-23 22:36:46 +0530

    Create Union of Two Sorted Arrays.cpp

---
+++ Union of Two Sorted Arrays.cpp
@@ -0,0 +1,25 @@
+class Solution{
+    public:
+    //arr1,arr2 : the arrays
+    // n, m: size of arrays
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+       unordered_set<int> s;
+       vector<int> v;
+       for(int i=0;i<n;i++){
+           if (s.find(arr1[i]) == s.end()){
+               s.insert(arr1[i]);
+               v.push_back(arr1[i]);
+           }
+       }
+       for(int i=0;i<m;i++){
+           if (s.find(arr2[i]) == s.end()){
+               s.insert(arr2[i]);              
+               v.push_back(arr2[i]);
+           }
+       }
+       sort(v.begin(),v.end());
+       return v;
+    }
+};
