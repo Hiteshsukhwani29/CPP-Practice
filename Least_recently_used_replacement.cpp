d4c26bbe4ec95f7d0fdcc1e1002a4f8c39783b35
Hitesh Sukhwani <55627571+Hiteshsukhwani29@users.noreply.github.com>
2022-07-07 12:48:26 +0530

    Create Least_recently_used_replacement.cpp

---
+++ Least_recently_used_replacement.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int p[]={1,3,0,3,5,6,3};
+    int f=3;
+    int ans[3]={-1,-1,-1};
+    int x=0,ctr=0;
+    int lru=-1;
+    for(int k=0;k<7;k++){
+        int flag=0;
+        for(int i=0;i<f;i++){
+            if(p[k]==ans[i]){
+                flag=1;
+                lru=i;
+            }
+        }
+        if(x==3){
+            x=0;
+        }
+        if(flag==0){
+            cout<<endl<<p[k]<<" -- ";
+            if(x==lru){
+                x++;
+                if(x==3){
+                    x=0;
+                }
+            }
+            ans[x]=p[k];
+            x++;
+            ctr++;
+        }
+        if(flag==1){
+            cout<<endl<<p[k]<<" -- ";
+        }
+        if(flag==1){
+            cout<<"No Page Fault";
+        }
+        if(flag==0){
+            for(int i=0;i<f;i++){
+                if(ans[i]==-1){
+                    cout<<" ";
+                }
+                else{
+                    cout<<ans[i]<<" ";
+                }
+            }
+        }
+    }
+    cout<<"\nTotal page faults: "<<ctr<<endl;
+    return 0;
+}
