decf1df18f9571958c70a9ca708c6cd999c2b142
Hitesh Sukhwani <55627571+Hiteshsukhwani29@users.noreply.github.com>
2023-03-18 22:48:39 +0530

    Create

---
+++ Plus One.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;i--){
+            if(i==digits.size()-1)
+                digits[i]++;
+            if(digits[i]==10){
+                digits[i]=0;
+                if(i!=0){
+                    digits[i-1]++;
+                }
+                else{
+                    digits.push_back(0);
+                    digits[i]=1;
+                }
+            }
+        }
+        return digits;
+    }
+};
